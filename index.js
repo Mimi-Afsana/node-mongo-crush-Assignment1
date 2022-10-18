f485baff6adf51677cffeaf04d49564f543b2645
Mimi-Afsana <afsanamimi61220@gmail.com>
2022-10-18 16:28:55 +0800

    added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 const userRoutes = require("./routes/user.route");
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
